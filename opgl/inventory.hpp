936dd7ec1f1d99b45f2c5bd2778098d61b37c124
Ethan Naugle <46229017+37h4nn4ug13@users.noreply.github.com>
2023-02-16 23:18:41 -0500

    Add project files.

---
+++ opgl/inventory.hpp
@@ -0,0 +1,43 @@
+#ifndef INVENTORY_H
+#define INVENTORY_H
+#include "Block.hpp"
+#include <map>
+
+const enum {
+    DIMOND_ORE,
+    CONTAINER,
+    HAPPY_FACE,
+    BEDROCK, 
+    GRASS
+};
+
+class Inventory {
+private:
+    int maxBlocks;
+    std::map<int, int> blocks;
+
+public:
+    Inventory(int startnum = 10) {
+        blocks.insert({ DIMOND_ORE, startnum });
+        blocks.insert({ CONTAINER, startnum });
+        blocks.insert({ HAPPY_FACE, startnum });
+        blocks.insert({ BEDROCK, startnum });
+        blocks.insert({ GRASS, startnum });
+    }
+    int getBlockCount(int type) {
+        return blocks.at(type);
+    }
+    void addBlock(int type) { // Adds one block at a time. 
+        blocks.at(type) += 1;
+    }
+    // This will remove one block of a specified type from the inventory and return true if you have enough. If you do not have enough it will return false. 
+    bool removeBlock(int type) {
+        if (blocks.at(type) > 0) {
+            blocks.at(type)--;
+            return true;
+        }
+        return false;
+    }
+};
+
+#endif
